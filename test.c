4d9e8ff284b21eb5b229666da64e61eba77fc550
pape souka diouf <papesoukad@gmail.com>
2022-08-08 12:56:54 +0200

    enregistrement fichier test

---
+++ test.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main (){
+    int nombre;
+    printf( "Veuillez saisir un entier");
+    scanf(%d,&nombre);
+    if ( nombre < 0){
+        printf(" valeur saisie est negatif");
+    }
+    else{
+        printf(" valeur saisie positif");
+    }
+
+}
